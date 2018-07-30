8f5d138d29725120ebaed9f52969ca3e4448da45
London Lowmanstone <happylondon4@gmail.com>
2018-07-30 14:06:56 -0500

    Added missing documentation

--- pipeline/csv_class.py
+++ pipeline/csv_class.py
@@ -26,7 +26,7 @@ class CSV:
         '''
         A class to store the information of CSV files
 
-        arg: either a CSV object (in which case it the init function makes a duplicate)
+        arg: either a CSV object (in which case the init function makes a duplicate) or a file path that the CSV object should load its data from
         '''
 
         if isinstance(arg, CSV):
